877c059166481c42b9b13f2d652c9232720cc4c7
Cole Marsteller <cgm32547@gmail.com>
2019-03-01 13:33:54 -0800

    remove margin from body, add background color to html and render Menu component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import Game from './js/components/Game/Game'
+import Menu from './js/components/Menu/Menu'
+import Helmet from 'react-helmet';
+
 
 class App extends Component {
   render() {
     return (
-        <Game />
+      <div>
+        <Helmet>
+          <style>{`body { margin: 0; width:100%; height:100%}`}</style>
+          <style>{`html { height:100%; background-color: #707F72}`}</style>
+        </Helmet>
+        <Menu />
+      </div>
     );
   }
 }
