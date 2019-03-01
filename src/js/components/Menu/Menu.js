2264feff2b84facef9d361c265200a50fdf7010e
Cole Marsteller <cgm32547@gmail.com>
2019-03-01 13:33:19 -0800

    render body and header

---
+++ src/js/components/Menu/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Body from './Body/Body';
+import Header from './Header/Header';
+
+class Menu extends React.Component {
+
+  render() {
+    return (
+      <div>
+      <Header />
+      <Body />
+      </div>
+    );
+  }
+}
+
+export default Menu;
