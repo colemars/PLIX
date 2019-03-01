115c9dfb7f6b63e52bbf635059d93996a9931838
Cole Marsteller <cgm32547@gmail.com>
2019-03-01 11:42:52 -0800

    create Game component tree

---
+++ src/js/components/Game/Game.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Body from './Body/Body';
+
+class Game extends React.Component {
+
+  render() {
+    return (
+        <Body />
+    );
+  }
+}
+
+export default Game;
