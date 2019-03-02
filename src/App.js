8312d60560d6d761e7c81c702c56cfc017ee1ab1
Cole Marsteller <cgm32547@gmail.com>
2019-03-01 18:04:24 -0800

    add routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './js/components/Home/Home'
 import Helmet from 'react-helmet';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 
 class App extends Component {
@@ -8,10 +9,15 @@ class App extends Component {
     return (
       <div>
         <Helmet>
-          <style>{`body { margin: 0; width:100%; height:100%}`}</style>
+          <style>{`body { margin: 0; width:100%; height:100%;}`}</style>
           <style>{`html { height:100%; background-color: #707F72}`}</style>
         </Helmet>
-        <Home />
+        <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route component={Home} />
+        </Switch>
+        </BrowserRouter>
       </div>
     );
   }
