e55df7051f8133be054582673c4d3d6d25dfe42d
Cole Marsteller <cgm32547@gmail.com>
2019-03-01 16:37:17 -0800

    style selectedMenu component

--- src/js/components/Home/Body/SelectedMenu.js
+++ src/js/components/Home/Body/SelectedMenu.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
+import MenuButton from './MenuButton'
+import {v4} from 'uuid'
 
 const styles = {
-  root: {
-
-  },
   menu: {
     backgroundColor: 'white',
-    height: '30vh',
+    height: '35vh',
     width: '20vw',
-    borderRadius: '5px'
+    borderRadius: '2px',
+    borderBottom: '5px solid #B4CCB7',
+    padding: '3px'
+  },
+  test: {
+    boxSizing: 'border-box',
+    width: '100%',
+    height: '100%',
+    border: '1px solid red',
   },
   menuItem: {
-    marginLeft: '20px',
-    marginRight: '20px'
+    boxSizing: 'border-box',
+    width: '100%',
+    height: '100%',
+    padding: '5px',
+    fontSize: '1.5vw',
+    fontFamily: 'Rubik',
   }
 };
 
 function SelectedMenu(){
   return (
 
-        <div style={styles.menu}>PLIX.DEV</div>
+    <Grid style={styles.menu} container justify='center' spacing = {0}>
+        <Grid key={v4()} item xs = {12}>
+          <div style={styles.menuItem}>
+            How to Play
+          </div>
+        </Grid>
+    </Grid>
 
   );
 }
