5514e3eff197a3958aa8554ec621a0df3cbe044b
Cole Marsteller <cgm32547@gmail.com>
2019-03-01 18:03:49 -0800

    render Canvas behind UI

--- src/js/components/Home/Home.js
+++ src/js/components/Home/Home.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import Body from './Body/Body';
 import Header from './Header/Header';
+import Canvas from './Canvas/Canvas';
+import P5Wrapper from 'react-p5-wrapper';
+import Sketch from "../../sketches/Test";
 
+const styles = {
+  gamecontainer: {
+    width: '800px',
+    height: '600px',
+    backgroundColor: 'beige',
+    border: '1px solid black',
+    position: 'relative',
+    margin: '0 auto',
+  },
+  canvas: {
+      position: 'absolute',
+       zIndex: 1
+     },
+  gamestartscreen: {
+    position: 'relative',
+    zIndex: 2,
+  }
+}
 class Menu extends React.Component {
+
 
   render() {
     return (
       <div>
-      <Header />
-      <Body />
+        <P5Wrapper sketch={Sketch} />
+        <div style={styles.gamestartscreen}>
+          <Header />
+          <Body />
+        </div>
       </div>
     );
   }
