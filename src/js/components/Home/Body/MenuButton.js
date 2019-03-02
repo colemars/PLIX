e7a384162bf9cde88984bff22921c8224659de6c
Cole Marsteller <cgm32547@gmail.com>
2019-03-01 16:35:47 -0800

    create menuButton component

---
+++ src/js/components/Home/Body/MenuButton.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types';
+
+const styles = {
+  button: {
+    boxSizing: 'border-box',
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#353535',
+    textAlign: 'center',
+  },
+  text:{
+    color: 'white',
+    fontSize: '1.5vw',
+    fontFamily: 'Rubik',
+    position: 'relative',
+    paddingTop: '6px'
+  }
+};
+
+function MenuButton(props){
+  return (
+    <div style={styles.button}>
+      <div style={styles.text}>
+        {props.selection}
+      </div>
+    </div>
+  );
+}
+
+MenuButton.propTypes = {
+  selection: PropTypes.string.isRequired
+};
+
+
+export default MenuButton;
