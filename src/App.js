46e8b55500e9aa9086e1806ebf542ad199f64e64
Cole Marsteller <cgm32547@gmail.com>
2019-03-03 17:59:53 -0800

    change background color

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div>
         <Helmet>
           <style>{`body { margin: 0; width:100%; height:100%;}`}</style>
-          <style>{`html { height:100%; background-color: #707F72}`}</style>
+          <style>{`html { height:100%; background-color: #3D9CA8}`}</style>
         </Helmet>
         <BrowserRouter>
         <Switch>
