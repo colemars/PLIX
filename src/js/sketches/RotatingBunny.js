bb8b943c740f0c4eb83fff202f3f923b218ab015
Cole Marsteller <cgm32547@gmail.com>
2019-03-03 17:56:18 -0800

    create bunny and rotatingbunny sketches

---
+++ src/js/sketches/RotatingBunny.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withApp } from "react-pixi-fiber";
+import Bunny from "./Bunny";
+
+// http://pixijs.io/examples/#/basics/basic.js
+class RotatingBunny extends Component {
+  state = {
+    rotation: 0
+  };
+
+  componentDidMount() {
+    this.props.app.ticker.add(this.animate);
+  }
+
+  componentWillUnmount() {
+    this.props.app.ticker.remove(this.animate);
+  }
+
+  animate = delta => {
+    // just for fun, let's rotate mr rabbit a little
+    // delta is 1 if running at 100% performance
+    // creates frame-independent tranformation
+    this.setState(state => ({
+      ...state,
+      rotation: state.rotation + 0.1 * delta
+    }));
+  };
+
+  render() {
+    return <Bunny {...this.props} rotation={this.state.rotation} />;
+  }
+}
+RotatingBunny.contextTypes = {
+  app: PropTypes.object
+};
+
+export default withApp(RotatingBunny);
