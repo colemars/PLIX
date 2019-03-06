8ee194f51b6db30d8ac1400a274fb1bb9b84c9f5
Cole Marsteller <cgm32547@gmail.com>
2019-03-06 14:30:06 -0800

    import new action types

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,6 +1,7 @@
 import { ADD_ARTICLE } from "../constants/action-types";
 import { SELECT_MENU } from "../constants/action-types";
-
+import { TOGGLE_UI } from "../constants/action-types";
+import { PLAY_GAME } from "../constants/action-types";
 
 export function addArticle(payload) {
   return { type: ADD_ARTICLE, payload }
