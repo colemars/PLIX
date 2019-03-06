85d1218c57d3fabaa677b786dfe6a760fb366968
Cole Marsteller <cgm32547@gmail.com>
2019-03-06 14:28:30 -0800

    create UI component for Game that displays on click

---
+++ src/js/components/UI/UI.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Transition } from "react-spring";
+
+import { GAME_HEIGHT, GAME_WIDTH } from "../../config";
+
+const LEFT_UI_WIDTH = 50;
+
+class UI extends React.Component {
+  state = {
+    leftOffset: 0
+  }
+
+  componentDidMount() {
+    this.setState({ leftOffset: this.calculateLeftOffset() })
+
+    window.addEventListener('resize', () => {
+      this.setState({ leftOffset: this.calculateLeftOffset() })
+    })
+  }
+
+  calculateLeftOffset = () => {
+    return window.innerWidth / 2 - GAME_WIDTH / 2
+  }
+
+  render() {
+    console.log(this.props);
+    const { showUi } = this.props
+    const { leftOffset } = this.state
+    return (
+      <div>
+        {/* Left */}
+        <div
+          style={{
+            position: 'absolute',
+            left: leftOffset,
+            width: LEFT_UI_WIDTH,
+            height: GAME_HEIGHT,
+            backgroundColor: 'yellow',
+            opacity: 0.5
+          }}
+        />
+        {/* Top */}
+        <div>
+          <Transition
+            items={showUi}
+            from={{ marginTop: -100 }}
+            enter={{ marginTop: 0 }}
+            leave={{ marginTop: -100 }}
+          >
+            {show =>
+              show &&
+              (props => (
+                <div
+                  style={{
+                    ...props,
+                    position: 'absolute',
+                    width: GAME_WIDTH,
+                    height: 100,
+                    top: 0,
+                    backgroundColor: '#fcfcfc'
+                  }}
+                />
+              ))
+            }
+          </Transition>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return { showUi: state.showUi};
+};
+
+export default connect(mapStateToProps)(UI);
