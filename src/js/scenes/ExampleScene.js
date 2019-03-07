b7af77680898c5e6ac70d28f5a551a5d6803d93c
Cole Marsteller <cgm32547@gmail.com>
2019-03-07 08:28:37 -0800

    pre load images and display sky

--- src/js/scenes/ExampleScene.js
+++ src/js/scenes/ExampleScene.js
@@ -4,7 +4,21 @@ import { toggleUi } from "../actions/index";
 import Phaser from "phaser";
 
 export default class ExampleScene extends Phaser.Scene {
+
+  preload(){
+    this.load.image('sky', 'assets/images/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude',
+      'assets/dude.png',
+      { frameWidth: 32, frameHeight: 48 }
+    );
+  }
+
   create() {
+    console.log(this);
+    this.add.image(400, 300, 'sky');
     const text = this.add.text(250, 250, "Toggle UI", {
       backgroundColor: "white",
       color: "blue",
@@ -16,6 +30,7 @@ export default class ExampleScene extends Phaser.Scene {
     text.on("pointerup", () => {
       store.dispatch({ type: TOGGLE_UI });
     });
+
   }
 }
 
